669170c4dd217558fbc9845cebabeed36326a11a
KevinChena <z6256875@gmail.com>
2023-03-04 00:20:22 +0800

    停車費完成

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 int main()
 {
-    int n;
-    while (cin >> n) {
-        double km = n * 1.6;
-        cout << fixed << setprecision(1) << km << endl;
+    int inx, iny;
+    int outx, outy;
+    
+    cin >> inx >> iny;
+    cin >> outx >> outy;
+    int time = (outx * 60 + outy) - (inx * 60 + iny);
+    if(time<=120 && time>=0){
+        cout << time / 30 * 30 << endl;
     }
+    else if(time>120 && time<=240){
+        cout << (time-120) / 30 * 40+120 << endl;
+    }
+    else {
+        cout << (time - 240) / 30 * 60 + 120 + 160<< endl;
+    }
+    
         return 0;
 }
