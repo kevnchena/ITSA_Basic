f42b828994bbc9ab863e51555403d882df2e3173
KevinChena <z6256875@gmail.com>
2023-03-04 22:34:14 +0800

    月份錯誤更改

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,24 +9,24 @@ int main()
     cout << "請輸入月份: ";
     cin >> x;
     switch ( x ) {
-    case 1:
-    case 2:
     case 3:
-        cout << "Spring" << endl;
-        break;
     case 4:
     case 5:
-    case 6:
-        cout << "Summer" << endl;
+        cout << "Spring" << endl;
         break;
+    case 6:
     case 7:
     case 8:
-    case 9:
-        cout << "Autumn" << endl;
+        cout << "Summer" << endl;
         break;
+    case 9:
     case 10:
     case 11:
+        cout << "Autumn" << endl;
+        break;
     case 12:
+    case 1:
+    case 2:
         cout << "Winter" << endl;
         break;
     default:
