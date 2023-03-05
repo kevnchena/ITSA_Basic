8370a6b8cce7cf0c1a6bff40b92e5ac6b304280d
KevinChena <z6256875@gmail.com>
2023-03-05 12:00:31 +0800

    圓圈

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,15 @@
-﻿#include<iostream>
-#include<iomanip>
+﻿#include<iostream>  
+#include<iomanip>  
 using namespace std;
 
 int main()
 {
-    int n;
-    while (cin >> n) {
-        double km = n * 1.6;
-        cout << fixed << setprecision(1) << km << endl;
+    int x, y;
+    while (cin >> x >> y) {
+        if (x * x + y * y <= 10000)
+            cout << "inside" << endl;
+        else
+            cout << "outside" << endl;
     }
-        return 0;
+    return 0;
 }
