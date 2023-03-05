5a250380b8dfe0aef4db83f831d98b8723a2a4cd
KevinChena <z6256875@gmail.com>
2023-03-05 23:18:10 +0800

    矩陣反轉 註解

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,16 +5,16 @@ int main()
 {
     int col, row;
     int *a;
-    int count = 0;
     cin >> col >> row;
-    a=new int[col*row];
- 
-    for (int i = 0; i < (col*row); i++) {
-       cin >> a[i];
+    a=new int[col*row];//配一個可以放 int值 的記憶體空間
+    
+    for (int i = 0; i < (col * row); i++) {
+       cin >> a[i];//輸入陣列元素
     }
-    for (int i = 0; i < row; i++) {
+    for (int i = 0; i < row; i++) { //列轉行
+        int count = 0;
 
-        for (int j = 0; j < col; j++) {
+        for (int j = 0; j < col; j++) {//行轉列
             count++;
                 cout << a[j * row + i];
                 if (count % col != 0)cout << " ";
