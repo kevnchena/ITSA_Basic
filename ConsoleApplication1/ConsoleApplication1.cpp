1b546fbc65f473129351530855da68c08b11aa4a
KevinChena <z6256875@gmail.com>
2023-03-16 12:07:22 +0800

    test string funtion

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,29 @@
 ﻿#include<iostream>
 using namespace std;
 
-void compare(str a;)
+//void compare(char a)
 
 int main()
 {
-	int n;
-	cin >> n >> endl;
+	/*int n;
+	cin >> n;
+	char a[50];
+	cin >> a;
+	cout << a << endl;*/
+
+	//它們的ASCII 排序剛好是大到小
+
+	/*cout << "S" << " " << (int)'S' << endl;
+	cout <<"H" << " " << (int)'H'<< endl;
+	cout <<"D" << " " << (int)'D5' << endl;
+	cout <<"C" << " " << (int)'C5' << endl;*/
+
+	char c[10];
+
+	while (true)
+	{
+		cin.getline(c,100);
+		cout << "得到"<< c  <<" "<<(int)c[0]<<(int)c[1] << endl; }
 	
-	
+	return 0;
 }
