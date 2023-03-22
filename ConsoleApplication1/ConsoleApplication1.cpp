a9f444a29eaa78b677aa478719efbb52c5adc27f
KevinChena <z6256875@gmail.com>
2023-03-22 22:31:33 +0800

    錯誤

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,26 @@
 ﻿#include<iostream>
+#include<string>
 using namespace std;
 
 int main()
 {
+	string input, output;
+	bool a = 1;
+
+	cin >> input;
+	int t = input.length()-1;
+	int count = 0;
+	for (int i = t; i >= 0; i--)//將字串逆序傳到output 
+	{	
+		output[count] = input[i];
+		count++;
+	}
+	for (int j = 0; j < t; j++)//比較是否有異 
+	{
+		if (input[j] != output[j]) { a = 0; break; }//發現有異即時跳出 
+		else a = 1;
+	}
+	a ? cout << "YES" << endl : cout << "NO" << endl;//輸出結果 
+	return 0;
+
 }
