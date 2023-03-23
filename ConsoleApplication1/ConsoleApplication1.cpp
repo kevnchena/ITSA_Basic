8370c5321e3d36eeac9b5d1f2510ab0c1cb0ca97
KevinChena <z6256875@gmail.com>
2023-03-23 17:29:51 +0800

    unfinished

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,34 @@
 ﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
 using namespace std;
+
+void split(const string& s,char delim,vector<string>& cards) {
+	stringstream ss(s);
+	string item;
+	while()
+
+}
+
+void sort(vector<string>)
 
 int main()
 {
+	int n;
+	cin >> n;
+	vector<strinig> cardDeck[10];
+	cin.ignore();
+
+	for (int i = 0; i < n; i++) 
+	{
+		string cardString;
+		
+		getline(cin, cardString);//可以讀空格
+		split(cardString, '', cardDeck[i]);
+		sort(cardDeck[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		display(card)
+	}
 }
