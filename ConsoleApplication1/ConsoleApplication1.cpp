7d701f823a9d110742b160250b360c53e1e974c7
KevinChena <z6256875@gmail.com>
2023-03-28 22:10:25 +0800

    1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,19 @@
 ﻿#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
 using namespace std;
 
+void sort(vector<string>)
+
 int main()
-{
+{	
+	int n;
+	string input[100];
+	int 
+
+	getline(cin, input[100]);
+	cin.ignore();
+	
+	
 }
