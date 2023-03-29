a0ae0949e5155d87fb1664dd399cc3353b0051be
KevinChena <z6256875@gmail.com>
2023-03-30 01:12:28 +0800

    提交

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,7 +13,7 @@ string checked(const string& obj) {//檢查有沒有超過1000字元
 }
 
 int main()
-{	
+{
 
 	string input, sort, output;
 	vector<string> container;
@@ -23,33 +23,34 @@ int main()
 	string checked(input);
 
 	istringstream str(checked);
-	
-	
 
-	while (getline(str,sort,' ')) {//分割成每碰到一個' '輸入一段字串
+
+
+	while (getline(str, sort, ' ')) {//分割成每碰到一個' '輸入一段字串
 		container.push_back(sort);
 	}
 
 	int limiter = container.size();
-	int count = container.size()-1;//計算要幾個空白 尾巴-1
+	int count = container.size() - 1;//計算要幾個空白 尾巴-1
 
 	for (int i = 0; i < limiter; i++) {
-		
+
 
 		for (int j = i + 1; j < limiter; j++) {//比較元素有沒有相等
 			if (container[i] == container[j]) {
-				
+
 				container[j].clear();//有就把當下的刪除
 				count--;
 			};
 		};
-		
+
 		if (!container[i].empty()) {//如果comtaimer[i]內值不空
 			cout << container[i];
 			if (i < count)cout << " ";//i<計算次數就不再輸出空白
 		};
-		
+
 	};
 	//cout << endl;	
 	return 0;
 }
+
