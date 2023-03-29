d61768b33fb9b2ab492133de69a05577fa800606
KevinChena <z6256875@gmail.com>
2023-03-29 23:48:46 +0800

    vector string.compare

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,7 +11,7 @@ int main()
 	string input,sort;
 	vector<string> container;
 	getline(cin, input);
-	transform(input.begin(), input.end(), input.begin(), tolower);
+	transform(input.begin(), input.end(), input.begin(), ::tolower);
 
 	istringstream str(input);
 
