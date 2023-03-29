dfda62260e8b52a09c554f6093c32dc0a775424f
KevinChena <z6256875@gmail.com>
2023-03-29 18:09:57 +0800

    A

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,13 @@
 ﻿#include<iostream>
+#include<string>
+#include<sstream>
 using namespace std;
 
 int main()
 {
+	string input, compare;
+	stringstream str1, str2;
+	getline(cin, input);
+	getline(cin, compare);
+
 }
