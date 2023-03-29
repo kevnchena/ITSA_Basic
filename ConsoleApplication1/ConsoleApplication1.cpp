7d17e2b24d459d180b4936c5c517349e2cc0d57b
KevinChena <z6256875@gmail.com>
2023-03-29 20:45:16 +0800

    測試成功 ITSA沒過

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,11 +10,12 @@ int main()
 	getline(cin, input);
 	getline(cin, input2);
 	
-	string::size_type pos = 0;
+	string::size_type pos = 0;//size_type是string::find的返回類型,沒用的話編譯失敗
 	int count=0;
 	if (input.length() < 128 && input2.length() < 512) {
+
 		while ((pos = input2.find(input, pos)) != string::npos)
-		{
+		{//        被檢查的字串.find(想找的字串,第幾個開始)!=string::no position
 			pos++;
 			count++;
 
