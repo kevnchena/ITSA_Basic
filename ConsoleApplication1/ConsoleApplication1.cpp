7cbdab92fceb75521865e3ea2fb44b9728ab4a80
KevinChena <z6256875@gmail.com>
2023-03-29 23:44:02 +0800

    string.compare sstream vector運用

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	string input;
+	string input,sort;
 	vector<string> container;
 	getline(cin, input);
 	transform(input.begin(), input.end(), input.begin(), tolower);
@@ -16,9 +16,27 @@ int main()
 	istringstream str(input);
 
 	
-	for (int i = 0;i<input.size();i++) {
-		container.push_back(input);
+	while (getline(str,sort,' ')) {//分割成每碰到一個' '輸入一段字串
+		container.push_back(sort);
 	}
-	cout << container[0] << endl;
 
+	int limiter = container.size();
+
+	for (int i = 0; i < limiter; i++) {
+		
+
+		for (int j = i + 1; j < limiter; j++) {//比較元素有沒有相等
+			if (container[i] == container[j]) {
+				
+				container[j].clear();//有就把當下的刪除
+			};
+		};
+		
+		cout << container[i];
+		if (container[i].compare("")!=0)cout << " ";//如果comtaimer[i]內的值不相等(!=0)才輸出
+		//if (count >0)cout << container[i] << " ";
+		
+	};
+	cout << endl;
+	return 0;
 }
