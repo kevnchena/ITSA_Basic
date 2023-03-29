de56d31eff35a1852e7f80cca329c614e9b5b391
KevinChena <z6256875@gmail.com>
2023-03-29 20:22:37 +0800

    find解決

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,14 +1,44 @@
 ﻿#include<iostream>
 #include<string>
-#include<map>
 using namespace std;
 
 int main()
 {
-	string input, compare;
+	string str = "isisisis";
+	string str2 = "isis";
 	
-	getline(cin, input);
-	getline(cin, compare);
-	map<string, int> str1 = { input,1 };
+	//str.find("is");
+
+	/*size_t found = str.find("isis");
+	if (found != string::npos) {
+		cout << "found at" << found << endl;
+
+	}
+	size_t found1 = str.find_first_of("is");
+	size_t found2 = str.find_last_of("is");
+	cout << "found1 at" << found1 << endl;
+	cout << "found2 at" << found2 << endl;*/
+	//搜尋特定範圍
 	
+	/*int length = str.length();
+	int pos = 0;
+	
+	str1.find(str2.c_str(), pos, length) != string::npos ?
+		cout << length << " chars match from pos " << pos << endl :
+		cout << "no match!" << endl;*/
+
+	string s = "we are the lion";
+	string flag;
+	string::size_type pos;
+
+	flag = "e";
+	pos = 0;
+	int i = 1;
+	while ((pos = s.find(flag,pos))!= string::npos){
+	 cout << "position  " << i << " : " << pos << endl;
+		pos++;
+		i++;
+	};
+
+
 }
