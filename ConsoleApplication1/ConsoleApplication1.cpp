e7bec29d58b85267f54a2cc0e0a0cd2bfafd074f
KevinChena <z6256875@gmail.com>
2023-03-29 22:12:48 +0800

    awd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,13 +2,20 @@
 #include<string>
 #include<map>
 #include<sstream>
-#include<​algorithm​>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 
 int main()
 {
-	string str;
-	istringstream 
-
+	string input;
+	vector<string> container;
+	istringstream str(input);
+	
+	while (getline(cin, input)) {
+		container.push_back(input);
+	}
+	cout << container << endl;
+	break;
 }
