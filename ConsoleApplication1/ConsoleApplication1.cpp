342df85ac6f83bf0e2cd13eb43966c9a56976c6e
KevinChena <z6256875@gmail.com>
2023-03-30 12:52:54 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,26 @@
 ﻿#include<iostream>
 #include<map>
-
+#include<sstream>
 using namespace std;
 
 int main()
 {
 	int n;
-	cin >> n;
+	int i = 0;
 	map<int, int> time;
+	
+	cin >> n;
+	cin.get();//吸收最後輸入的enter鍵
+	string input_time,sort;
+	getline(cin, input_time);
+	stringstream ss(input_time);
 
+	while (getline(ss, sort, ' ')) {
+		if (i < n)time[i] =  (int)sort[i] + (int)sort[i + 1];
+		
+		i++;
+		cout << sort << endl;
+		cout << "time: " << time[i] << endl;
+	}
+	
 }
