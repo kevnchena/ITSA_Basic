0df30ba73a9f8ff3e402de3aa1f7d0c1bbf8dd8b
KevinChena <z6256875@gmail.com>
2023-03-30 11:36:20 +0800

    vector問題

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,32 @@
 ﻿#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
 using namespace std;
 
 int main()
-{
+{	
+	string a = "~!@#$%^&*()_++`1234567890-=={}||qwertyuiop[]\\:""asdffghjkl;''<>??zxcvbnm,.//";
+	int a_len = a.size();
+	cout << a.size();
+	
+	string input;
+	vector<string>output = {};
+
+	getline(cin, input);
+	int input_len = input.size();
+	
+	for (int i = 0; i < input_len; i++) {
+		if (input[i] == ' ')break;
+
+		for (int j = 0; j < a_len; j++) {
+			if (input[i] == a[j])
+				output[i].push_back(a[j + 1]);
+				
+		};
+
+	};
+	for (auto&& i:output) { cout << i; };
+	cout << endl;
+	return 0;
 }
