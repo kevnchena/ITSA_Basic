6e1c83825b2f645bc9805916e610ee1b4cf6a2d7
KevinChena <z6256875@gmail.com>
2023-03-30 12:04:07 +0800

    String 運用

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,31 +1,31 @@
 ﻿#include<iostream>
-#include<sstream>
 #include<string>
-#include<vector>
 #include<algorithm>
 using namespace std;
 
 int main()
-{	
+{	//a的宣告裡面以行左至右排列，尾巴的字元就多一次
 	string a = "~!@#$%^&*()_++`1234567890-=={}||qwertyuiop[]\\:""asdffghjkl;''<>??zxcvbnm,.//";
 	int a_len = a.size();
 	
 	string input;
 	string output[127];
-	getline(cin, input);
+	getline(cin, input);//可以讀spacebar
 	transform(input.begin(), input.end(), input.begin(), tolower);
 	int input_len = input.size();
 	
-	for (int i = 0; i < input_len; i++) {
+	for (int i = 0; i < input_len; i++) {//input長度為外迴圈容量
 
-		for (int j = 0; j < a_len; j++) {
-			if (input[i] == ' ') {
+		for (int j = 0; j < a_len; j++) {//鍵盤長度
+
+			if (input[i] == ' ') {//空白就等於空白
+
 				output[i] = ' ';
 				break;
 			}
-			else if (input[i] == a[j]) {
-				output[i] = a[j + 1];
-				break;
+			else if (input[i] == a[j]) {//輸入比鍵盤
+				output[i] = a[j + 1];//一樣就+1
+				break;//只做一次
 			};
 		};
 		cout << output[i];
