69cb8ad78f3eab093a3db97da336d5d957f34ce6
KevinChena <z6256875@gmail.com>
2023-04-01 00:15:57 +0800

    String

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,22 @@
 ﻿#include<iostream>
+#include<string>
 using namespace std;
+
+void compare(string &num1, string &num2) {
+	string temp;
+	if (num1.size() < num2.size()) {
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+
+	};
+	
+}
 
 int main()
 {
+	string num1="0", num2="11";
+	cout << num1 << num2 << endl;
+	compare(num1, num2);
+	cout << num1 << num2 << endl;
 }
