bc52905d37e92fb2833595a24a19fd8240d8ea84
KevinChena <z6256875@gmail.com>
2023-04-04 17:17:52 +0800

    double

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main()
 {
     long double r, n, p;
-    double sum = 0.0;
+    double sum = 0.0;//本利和
     cin >> r >> n >> p;
     r += 1.0;
+    
     for (int i = 1; i <= n; i++) {
 
-        sum = (sum + p) * r;
+        sum = (sum + p) * r;//(前面+本次存款)*利率
     }
-    cout << (long long)sum << endl;
+    cout << (long long)sum << endl;//longlong可以忽略掉小數點
 
     return 0;
 }
