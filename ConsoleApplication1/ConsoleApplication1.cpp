452a97fd0fefe97bf321499df70ee900cefe4918
KevinChena <z6256875@gmail.com>
2023-04-04 17:48:21 +0800

    重來

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,14 +6,19 @@ using namespace std;
 int main()
 {
     double a[10];
-    double max = 0.0, min = 0.0;
+    double max=0.0, min=0.0;
     for (int i = 0; i < 10; i++) {
         cin >> a[i];
+    }
+    max = a[0];
+    min = a[0];
+   for (int i = 0; i < 10; i++) {
         if (a[i] > max)max = a[i];
         if (a[i] < min)min = a[i];
 
     }
+
     cout << "maximum:" << fixed << setprecision(2) << max << endl;
-    cout << "maximum:" << fixed << setprecision(2) << min << endl;
+    cout << "minimum:" << fixed << setprecision(2) << min << endl;
     return 0;
 }
