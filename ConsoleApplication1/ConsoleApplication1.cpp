1d0d842eefd6da81a335106efe6d88057e6a0e12
KevinChena <z6256875@gmail.com>
2023-04-04 17:11:13 +0800

    jp4wuj6

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,21 @@
 ﻿#include<iostream>
+#include<bitset>
 using namespace std;
+
+
 
 int main()
 {
+	float r,n,p;
+	float sum = 0.0;
+	cin >> r >> n >> p;
+	r += 1.0;
+	cout << endl;
+	for (int i = 1; i <= n; i++) {
+
+		sum =(sum+ p) * r;
+	}
+	cout << (long long)sum << endl;
+
+	return 0;
 }
