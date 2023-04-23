73ad9733ce2c0998deb1f8b181ba6a71c7bee81b
KevinChena <z6256875@gmail.com>
2023-04-24 01:15:52 +0800

    通過

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,20 +5,19 @@ using namespace std;
 int main()
 {
 	string answer, input;
-	string endcode = "0000";
-	int A, B;
-	cin >> answer;
+	int A, B;//計算器
+	cin >> answer;//答案
 
 	do {
-		cin >> input;
-		if (input == "0000")break;
-		A = 0;
+		cin >> input;//輸入
+		if (input == "0000")break;//要放在cin下一列最好
+		A = 0;//reset
 		B = 0;
 		for (int i = 0; i < 4; i++) {
-			if (answer[i] == input[i])A++;
+			if (answer[i] == input[i])A++;//當下相等就A++
 
 			else {
-				for (int j = 0; j < 4; j++)if (answer[i] == input[j]) B++;
+				for (int j = 0; j < 4; j++)if (answer[i] == input[j]) B++;//尋找字串中相等的數
 			}
 		}
 	cout << A << 'A' << B << 'B' << endl;
