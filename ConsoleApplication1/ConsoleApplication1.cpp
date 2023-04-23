f9e451e377d92e8eac1d2467ebeb52d083bafd7d
KevinChena <z6256875@gmail.com>
2023-04-24 01:46:15 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,4 +3,24 @@ using namespace std;
 
 int main()
 {
+	string special, jp1, jp2, jp3;
+	int n, c;
+	string ticket;
+	int hit[7] = { 0 };
+	int sum = 0;
+	cin >> special >> jp1 >> jp2 >> jp3;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> ticket;
+		
+		do {
+			if (ticket[c] == special[c] || ticket[c] = jp1[c]);
+
+
+		} while (c != 0);
+	}
+
+
+
+
 }
