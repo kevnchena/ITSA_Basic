75bebd4c4e910e12b1be256a5f649e880606227e
KevinChena <z6256875@gmail.com>
2023-04-23 17:30:58 +0800

    測試不過?

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,23 @@
 ﻿#include<iostream>
+#include<string>
+
+
 using namespace std;
 
-int main()
-{
+int main(){
+	int n, sum = 0;
+	string str;
+	cin >> n;
+	cin.ignore();
+
+	for (int i = 0; i < n; i++) {
+		getline(cin,str);
+		for (int j = str.length() - 1; j>=0; j--) {
+			sum += int(str[j]);
+			
+		}
+		cout << sum << endl;
+		sum = 0;
+	};
+
 }
