bb0d6760171ec366d44f7b2e81371a7eec0edaaf
KevinChena <z6256875@gmail.com>
2023-04-25 01:28:29 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -21,15 +21,15 @@ int main()
 	string special, jp1, jp2, jp3;
 	int n;
 	string ticket;
-	int hit[3] = { 0 };//各獎項中獎次數 0是特別獎 1,2,3是頭獎，所以才會多兩個
+	int hit[3] = { 0 };//頭獎中的號碼數量
 	int prize[7] = { 0 };//獎項
-	int sum = 0;
+	int sum = 0;//最後價格
 	cin >> special >> jp1 >> jp2 >> jp3;
-	cin >> n;
+	cin >> n;//發票數量
 	for (int i = 0; i < n; i++) {
 		cin >> ticket;
 		
-		if (ticket == special) {
+		if (ticket == special) {//特別獎有中就直接break;
 			prize[0]++;
 			break;
 		}
@@ -41,9 +41,9 @@ int main()
 			check(ticket, jp3, hit, 2);
 		}
 		
-		if (hit[0] >= hit[1] && hit[1] >= hit[2])prize[6- hit[0]+3]++;
-		else if (hit[1] >= hit[0] && hit[0] >= hit[2])prize[6 - hit[1]+3]++;
-		else if (hit[2] >= hit[0] && hit[0] >= hit[1])prize[6 - hit[2]+3]++;
+		if (hit[0] >= hit[1] && hit[0] >= hit[2])prize[6- hit[0]+3]++;
+		else if (hit[1] >= hit[0] && hit[1] >= hit[2])prize[6 - hit[1]+3]++;
+		else if (hit[2] >= hit[0] && hit[2] >= hit[1])prize[6 - hit[2]+3]++;
 		hit[0] = { 0 };
 		hit[1] = { 0 };
 		hit[2] = { 0 };
@@ -59,10 +59,6 @@ int main()
 			cout << endl;
 			sum += prize[0] * 2000000 + prize[1] * 200000 + prize[2] * 40000 + prize[3] * 10000 +
 				prize[4] * 4000 + prize[5] * 1000 + prize[6] * 200;
-			cout << sum;
+			cout << sum<<endl;
 	
-
-
-
-
 }
