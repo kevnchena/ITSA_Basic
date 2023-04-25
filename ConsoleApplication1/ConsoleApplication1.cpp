9c29a41879ab149af10f4c82f66487e55665f167
KevinChena <z6256875@gmail.com>
2023-04-26 02:34:42 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,29 +5,44 @@ using namespace std;
 int main()
 {	
 	string input;
+	getline(cin, input);
 	int num = input[0] - '0' + input[1] - '0';
-	switch (num) {
-	case 3:
-		cout << "Aquarius" << endl;
-	case 5:
-		cout << "Pisces" << endl;
-	case 7:
-		cout << "Aries" << endl;
-	case 9:
-		cout << "Taurus" << endl;
-	case 11:
-		cout << "Gemini" << endl;
-	case 13:
-		
-	case 15:
-		cout << "Leo" << endl;
-	case 17:
-		cout << "Virgo" << endl;
-	case 19:
-		cout << "Libra" << endl;
-	case 21:
-		cout << "Scorpio" << endl;
-	case 23:
-		cout << "Sagittarius" << endl;
+	int num2 = input[3] - '0' + input[4] - '0';
+	if (num == 13) {
+		cout<<"yes" << endl;
+	}
+	else {
+		switch (num) {
+		case 3:
+			cout << "Aquarius" << endl;
+			break;
+		case 5:
+			cout << "Pisces" << endl;
+			break;
+		case 7:
+			cout << "Aries" << endl;
+			break;
+		case 9:
+			cout << "Taurus" << endl;
+			break;
+		case 11:
+			cout << "Gemini" << endl;
+			break;
+		case 15:
+			cout << "Leo" << endl;
+			break;
+		case 17:
+			cout << "Virgo" << endl;
+			break;
+		case 19:
+			cout << "Libra" << endl;
+			break;
+		case 21:
+			cout << "Scorpio" << endl;
+			break;
+		case 23:
+			cout << "Sagittarius" << endl;
+			break;
+		}
 	}
 }
