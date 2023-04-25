903d587007f5da0af175c3ee35ebba372161b8d5
KevinChena <z6256875@gmail.com>
2023-04-26 02:27:25 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,33 @@
 ﻿#include<iostream>
+#include<string>
 using namespace std;
 
 int main()
-{
+{	
+	string input;
+	int num = input[0] - '0' + input[1] - '0';
+	switch (num) {
+	case 3:
+		cout << "Aquarius" << endl;
+	case 5:
+		cout << "Pisces" << endl;
+	case 7:
+		cout << "Aries" << endl;
+	case 9:
+		cout << "Taurus" << endl;
+	case 11:
+		cout << "Gemini" << endl;
+	case 13:
+		
+	case 15:
+		cout << "Leo" << endl;
+	case 17:
+		cout << "Virgo" << endl;
+	case 19:
+		cout << "Libra" << endl;
+	case 21:
+		cout << "Scorpio" << endl;
+	case 23:
+		cout << "Sagittarius" << endl;
+	}
 }
