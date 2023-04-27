10317521d2d6d30967fd87e66262a71df7cfd654
KevinChena <z6256875@gmail.com>
2023-04-27 13:30:05 +0800

    通過

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,48 +1,47 @@
 ﻿#include<iostream>
 #include<string>
+#include<map>
 using namespace std;
+
+
 
 int main()
 {	
 	string input;
 	getline(cin, input);
-	int num = input[0] - '0' + input[1] - '0';
-	int num2 = input[3] - '0' + input[4] - '0';
-	if (num == 13) {
-		cout<<"yes" << endl;
+	int month = (input[0] - '0')*10 + input[1] - '0';
+	int day = (input[3] - '0')*10 + input[4] - '0';
+	int sum = month * 30 + day;
+	if (sum >= 382 || sum <= 50) {
+		cout << "Capricorn" << endl;
+	}else if (sum <= 78) {
+		cout << "Aquarius" << endl;
 	}
-	else {
-		switch (num) {
-		case 3:
-			cout << "Aquarius" << endl;
-			break;
-		case 5:
-			cout << "Pisces" << endl;
-			break;
-		case 7:
-			cout << "Aries" << endl;
-			break;
-		case 9:
-			cout << "Taurus" << endl;
-			break;
-		case 11:
-			cout << "Gemini" << endl;
-			break;
-		case 15:
-			cout << "Leo" << endl;
-			break;
-		case 17:
-			cout << "Virgo" << endl;
-			break;
-		case 19:
-			cout << "Libra" << endl;
-			break;
-		case 21:
-			cout << "Scorpio" << endl;
-			break;
-		case 23:
-			cout << "Sagittarius" << endl;
-			break;
-		}
+	else if (sum <=110 ) {
+		cout << "Pisces" << endl;
+	}
+	else if (sum <= 140) {
+		cout << "Aries" << endl;
+	}
+	else if (sum <=171 ) {
+		cout << "Taurus" << endl;
+	}
+	else if (sum <=201) {
+		cout << "Gemini" << endl;
+	}
+	else if (sum <=232) {
+		cout << "Cancer" << endl;
+	}
+	else if (sum <= 263) {
+		cout << "Leo" << endl;
+	}
+	else if (sum <= 293) {
+		cout << "Virgo" << endl;
+	}
+	else if (sum <= 352) {
+		cout << "Scorpio" << endl;
+	}
+	else if (sum <= 381) {
+		cout << " Sagittarius" << endl;
 	}
 }
