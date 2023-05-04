55ee14c6a0af4a78038c39917dbb34a520ef901a
KevinChena <z6256875@gmail.com>
2023-05-04 14:41:57 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main()
 {
 	int input, i = 0;
-	int num[11] = { 0 };
+	int num[11] = {NULL};
 
 	while (cin >> input) {
 		num[i] = input;
 		cout << num[i];
 		i++;
 	}
-	S
+	for (int j = 0; j < 11; j++) {
+
+		for (int k = 0; k < 11; k++) {
+			if(num[k]!=0)
+		}
+	}
+	
 }
