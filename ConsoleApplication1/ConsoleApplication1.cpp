39c6491ca4bc5e307a841304f3523b216adbe5a2
KevinChena <z6256875@gmail.com>
2023-05-04 15:14:46 +0800

    asd

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,21 +1,25 @@
 ﻿#include<iostream>
+#include<vector>
 using namespace std;
 
 int main()
 {
 	int input, i = 0;
-	int num[11] = {NULL};
+	int a[10] = { 0 };
+	vector<int> num;
 
 	while (cin >> input) {
-		num[i] = input;
+		num.push_back(input);
 		cout << num[i];
 		i++;
 	}
-	for (int j = 0; j < 11; j++) {
+	/*for (int j = 0; j < 10; j++) {
 
-		for (int k = 0; k < 11; k++) {
-			if(num[k]!=0)
+		for (int k = 0; k < 10; k++) {
+			if (num[k] != 0&&num[j]==num[k]) {
+				a[j]++;
+			}
 		}
-	}
+	}*/
 	
 }
